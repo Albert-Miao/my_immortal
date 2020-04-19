8aa1b4ed0a55fa123bf74689c1f3f9395fb929de
Aidan Strong <aidan.m.strong@gmail.com>
2020-04-18 17:24:33 -0700

    Added basic security guard coding, and basic player model, stored in a scene

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private PolygonCollider2D collider2D;
+
+    public float speed=1;
+
+    private bool SPRITE_RIGHT = false;
+    private bool SPRITE_LEFT = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+        collider2D = this.GetComponent<PolygonCollider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the boyo
+        float dx = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float dy = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.position = new Vector2(transform.position.x+dx, transform.position.y+dy);
+        if(dx>0){
+            this.GetComponent<SpriteRenderer>().flipX=SPRITE_RIGHT;
+        }
+        else if (dx<0){
+            this.GetComponent<SpriteRenderer>().flipX=SPRITE_LEFT;
+        }
+    }
+
+}
---
+++ Assets/securityGuardController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class securityGuardController : MonoBehaviour
+{
+
+    private Rigidbody2D rigidbody;
+    private PolygonCollider2D collider2D;
+
+    public float speed=1;
+    private float direction;
+    private float swapTimer=5;
+    private float time=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Random.Range(-2*Mathf.PI,2*Mathf.PI);
+        rigidbody = this.GetComponent<Rigidbody2D>();
+        collider2D = this.GetComponent<PolygonCollider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(time>swapTimer){
+            direction = Random.Range(-2*Mathf.PI,2*Mathf.PI);
+            time=0;
+        }
+        time+=Time.deltaTime;
+
+        //move the boyo
+        float dy = Mathf.Sin(direction) * speed * Time.deltaTime;
+        float dx = Mathf.Cos(direction) * speed * Time.deltaTime;
+        transform.position = new Vector2(transform.position.x+dx, transform.position.y+dy);
+        transform.rotation = Quaternion.Euler(0, 0, direction*360f/2*Mathf.PI);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Player") { Destroy(other.gameObject);}
+
+
+    }
+}
